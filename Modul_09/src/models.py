de37168ed453bbb2f1aa8956140c95abf72eb11c
Vladyslav Babenko <96999713+vlad-bb@users.noreply.github.com>
2022-09-03 16:43:25 +0300

    create alembic version

--- Modul_09/src/models.py
+++ Modul_09/src/models.py
@@ -2,3 +2,14 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
 
 from src.db import Base
+
+
+class Contact(Base):
+    __tablename__ = 'contacts'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    phone = Column('phone', String(20), nullable=False)
+    birthday = Column('birthday', Date, nullable=True)
+    email = Column('email', String(30), nullable=True)
+    address = Column('address', String(120), nullable=True)
+
